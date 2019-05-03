ed826e503ac0d702a5ec24703e9971330ab6a2ee
pattywitwhy <pattyxhwang@gmail.com>
2019-05-03 09:33:21 -0500

    main.ts and main.js

--- main.ts
+++ main.ts
@@ -1,7 +1,29 @@
-function log(message) {
-  console.log(message);
-}
+// function log(message) {
+//   console.log(message);
+// }
 
-var message = 'Hello World';
+// var message = 'Hello World';
 
-log(message);
+// log(message);
+
+
+// var number = 1;
+
+// let count = 2;
+
+
+// function doSomething() {
+//   for (var i = 0; i < 5; i ++) {
+//     console.log(i);
+//   }
+
+//   console.log('Finally: ' + i);
+// }
+
+// doSomething();
+
+let count = 5;
+count = 'a';
+
+let a;
+
